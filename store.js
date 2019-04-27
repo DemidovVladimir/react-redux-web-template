4bc2bd97f9fb997bb5c69fafdae31147ed6446ef
Vladimir Demidov <vladimirdemidov@Vladimirs-MacBook-Pro.local>
2019-04-27 17:10:17 +0200

    feat(redux-sagas): Add redux sagas

--- store.js
+++ store.js
@@ -1,15 +1,30 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import {exampleReducer} from "./reducers/example-reducer";
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { helloSaga } from './sagas/exampleSaga';
 
 const reducer = combineReducers({
     example: exampleReducer,
 });
 
-export function initializeStore () {
-  return createStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
-  )
+export function initializeStore() {
+    const sagaMiddleware = createSagaMiddleware();
+
+    const store = createStore(
+        reducer,
+        composeWithDevTools(
+            applyMiddleware(
+                thunkMiddleware,
+                sagaMiddleware,
+                logger
+            )
+        )
+    );
+    store.sagaTask = sagaMiddleware.run(helloSaga);
+
+    return store
 }
+
